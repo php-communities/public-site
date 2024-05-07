5c5173a6edf83a25f6b0ddb56b38bb1f5d47991b
Pavel Ermakov <pawelermakov@gmail.com>
2024-05-07 13:58:00 +0300

    feat: show more button to events

--- app/(home)/_components/events/cards.tsx
+++ app/(home)/_components/events/cards.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 
 import EventCard from '@/(home)/_components/events/card';
 import { Event } from '~/lib/types/events';
@@ -6,31 +8,47 @@ import { Content } from '~/lib/utils/read-content';
 
 type EventCardsProps = {
     cards: Content<Event>[];
+    count?: number;
 };
 
-function EventCards({ cards }: EventCardsProps) {
+function EventCards({ cards, count = 12 }: EventCardsProps) {
+    const [items, setItems] = useState(cards.slice(0, count));
+
     return (
         <>
             {cards.length > 0 && (
-                <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3 lg:gap-y-20">
-                    {cards.map(({ slug, content, data }: Content<Event>) => (
-                        <EventCard
-                            key={slug}
-                            slug={slug}
-                            date={data.date}
-                            title={data.title}
-                            text={content}
-                            link={data.link}
-                            tab={data.tab}
-                            filters={data.filters}
-                            city={data.city}
-                        />
-                    ))}
-                </div>
+                <>
+                    <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
+                        {items.map(({ slug, content, data }: Content<Event>) => (
+                            <EventCard
+                                key={slug}
+                                slug={slug}
+                                date={data.date}
+                                title={data.title}
+                                text={content}
+                                link={data.link}
+                                tab={data.tab}
+                                filters={data.filters}
+                                city={data.city}
+                            />
+                        ))}
+                    </div>
+
+                    {items.length < count ||
+                        (items.length < cards.length && (
+                            <button
+                                type="button"
+                                className="mx-auto mt-16 block h-[45px] rounded border border-blue-dark bg-white px-8 font-inter text-sm font-bold leading-[45px] text-blue-dark transition hover:bg-blue-dark hover:text-white"
+                                onClick={() => setItems(cards)}
+                            >
+                                Еще...
+                            </button>
+                        ))}
+                </>
             )}
 
             {cards.length === 0 && (
-                <div className="flex min-h-[378px] w-full items-center justify-center border border-gray">
+                <div className="flex min-h-[378px] w-full items-center justify-center rounded border border-gray">
                     <div className="font-inter font-light text-gray">Ничего не найдено</div>
                 </div>
             )}
