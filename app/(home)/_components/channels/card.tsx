2440099771321276ac649b8079ceba36e8ebeccd
Pavel Ermakov <pawelermakov@gmail.com>
2024-05-13 12:36:36 +0300

    fix: sort channels by slug

--- app/(home)/_components/channels/card.tsx
+++ app/(home)/_components/channels/card.tsx
@@ -13,8 +13,8 @@ export type ChannelsProps = {
 function ChannelsCard({ imgSrc, title, text, link }: ChannelsProps) {
     return (
         <Link href={link} target="_blank" className="group">
-            <div className="mb-[20px]">
-                <Image className="object-cover" src={imgSrc} alt={title} width={266} height={266} />
+            <div className="mb-[20px] aspect-square">
+                <Image className="object-cover" src={imgSrc} alt={title} width={254} height={254} />
             </div>
             <div className="mb-[12px] block text-[18px] font-bold leading-[1.35] text-blue-dark underline underline-offset-4 transition group-hover:text-blue-light">
                 {title}
--- app/(home)/_components/channels/index.tsx
+++ app/(home)/_components/channels/index.tsx
@@ -8,15 +8,22 @@ import {
 } from '@/_components/core/accordion/accordion';
 import ChannelsCard, { ChannelsProps } from '@/(home)/_components/channels/card';
 import { basePath } from '~/lib/consts/base-path.mjs';
+import { sortBySlug } from '~/lib/utils/helpers';
 import { readContent } from '~/lib/utils/read-content';
 
 export function Channels() {
-    const news = readContent<ChannelsProps>('channels/news/*');
-    const frameworks = readContent<ChannelsProps>('channels/frameworks/*');
-    const chats = readContent<ChannelsProps>('channels/chats/*');
-    const podcasts = readContent<ChannelsProps>('channels/podcasts/*');
-    const youtube = readContent<ChannelsProps>('channels/youtube/*');
-    const jobs = readContent<ChannelsProps>('channels/jobs/*');
+    const news = readContent<ChannelsProps>('channels/news/*').sort(sortBySlug<ChannelsProps>);
+    const frameworks = readContent<ChannelsProps>('channels/frameworks/*').sort(
+        sortBySlug<ChannelsProps>
+    );
+    const chats = readContent<ChannelsProps>('channels/chats/*').sort(sortBySlug<ChannelsProps>);
+    const podcasts = readContent<ChannelsProps>('channels/podcasts/*').sort(
+        sortBySlug<ChannelsProps>
+    );
+    const youtube = readContent<ChannelsProps>('channels/youtube/*').sort(
+        sortBySlug<ChannelsProps>
+    );
+    const jobs = readContent<ChannelsProps>('channels/jobs/*').sort(sortBySlug<ChannelsProps>);
 
     return (
         <div className="w-full pb-[45px] lg:scroll-mt-[68px] lg:pb-[90px]" id="chat">
